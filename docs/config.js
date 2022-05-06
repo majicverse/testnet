e15b567790a9a16d4cc0d82ebca18be9839ef4fc
th <1450096592@qq.com>
2022-05-07 00:40:58 +0800

    bate-0.0.1

---
+++ docs/config.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const config = {
+  network: {
+    chainId: "0x61",
+    chainName: "BSC Testnet",
+    nativeCurrency: {
+      name: "BNB",
+      symbol: "BNB",
+      decimals: 18,
+    },
+    rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545"],
+  },
+  captainContracts: "0xd0366fd8cc25a517177c5082e311758d52065f43", // 舰长
+  majicArkContracts: "0x4D093871A088c2E98dB72A3CaacC7E23DD0cE3Fd", // 马吉
+  seaOfFreedomContracts: "0x05d4d03748224788072D7c2FA9Bb5D708b094b34", // 自由
+};
